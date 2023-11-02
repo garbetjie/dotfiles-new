9dc406ba6b2954c399d510d9621385d03b36f585
Geoff Garbers <geoff@Geoff-Garbers.local>
2023-11-02 21:32:44 +0200

    Add .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Set shared variables.
+set brew_prefix "/opt/homebrew"
+
+# Modify $PATH
+fish_add_path $brew_prefix"/bin"
+
+# Source in required files.
+source $brew_prefix"/opt/asdf/libexec/asdf.fish"
+
+# Start up starship.
+starship init fish | source
